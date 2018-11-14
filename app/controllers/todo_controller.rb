f0038ffe88f66e86d4da6b9eaecaad853ffe1f13
Yvonne Chan <y.bonchan@gmail.com>
2018-11-14 22:09:25 +0000

    Define instance variables in Todo show action, and place in show view

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,8 @@
 class TodoController < ApplicationController
     def index
     end
+    def show
+        @todo_description = "Make the curriculum"
+        @todo_pomodoro_estimate = 4
+    end
 end
