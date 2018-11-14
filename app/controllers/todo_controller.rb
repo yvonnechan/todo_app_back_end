b33abe84fb8c12787bb4df73ba7b531e19388223
Yvonne Chan <y.bonchan@gmail.com>
2018-11-14 22:49:35 +0000

    Add links to todos on the index page

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,18 +2,9 @@ class TodoController < ApplicationController
     def index
     end
     def show
-        @todo_description = "Make the curriculum"
-        @todo_pomodoro_estimate = 4
+        @todo_description = "Nothing"
+        @todo_pomodoro_estimate = 19
         
           todo_id = params[:id]
-
-  if todo_id == '1'
-    @todo_description = "Make the curriculum"
-    @todo_pomodoro_estimate = 4
-
-  elsif todo_id == '2'
-    @todo_description = "Buy workshop supplies"
-    @todo_pomodoro_estimate = 3
-  end
     end
 end
