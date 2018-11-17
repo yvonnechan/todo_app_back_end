f8c31c6f1c21428c46f95f46a75033e2696acfea
Yvonne Chan <y.bonchan@gmail.com>
2018-11-17 19:21:46 +0000

    Install postgres and update database.yml

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,9 +2,14 @@ class TodoController < ApplicationController
     def index
     end
     def show
+        todo_id = params[:id]
         @todo_description = "Nothing"
         @todo_pomodoro_estimate = 19
         
-          todo_id = params[:id]
+        if todo_id == "1" 
+            @todo_description = "Nothing"
+            @todo_pomodoro_estimate = 19
+        end
+          
     end
 end
