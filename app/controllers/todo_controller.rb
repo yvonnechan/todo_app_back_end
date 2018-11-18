848be61972927eeab31cfc06e2f5ce079a2e511c
Yvonne Chan <y.bonchan@gmail.com>
2018-11-18 19:54:39 +0000

    Add the edit, update, and destroy RCAV flows

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -13,4 +13,19 @@ class TodoController < ApplicationController
         t.save
         redirect_to "/todo/show/#{ t.id }"
     end
+    def edit
+        @todo = Todo.find_by_id(params[:id])
+    end
+    def update
+        t = Todo.find_by_id(params['id'])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+    end
+    def destroy
+        t = Todo.find_by_id(params[:id])
+        t.destroy
+        redirect_to "/todo/index"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
     get 'todo/index', to: 'todo#index'
     get 'todo/show/:id', to: 'todo#show'
     get 'todo/new', to: 'todo#new'
-    get 'todo/create', to 'todo#new'
+    get 'todo/create', to: 'todo#create'
+    get 'todo/edit/:id', to: 'todo#edit'
+    get 'todo/update/:id', to: 'todo#update'
+    get 'todo/destroy/:id', to: 'todo#destroy'
 end
